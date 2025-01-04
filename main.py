bdcfc9b0c352d59432d0f7037ab13452f315d79d
Muhammad Suleman <iam.suleman.m@gmail.com>
2025-01-04 23:32:27 +0500

    Simple Calculator

---
+++ main.py
@@ -0,0 +1,18 @@
+#Simple Calculator
+
+firstVal = float(input("Add your first value: "));
+lastVal = float(input("Add your last value: "));
+operator = input("Add your operator(+ - / *): ");
+
+if operator == '+':
+      print(firstVal + lastVal);
+elif operator == '-':
+      print(firstVal - lastVal);
+elif operator == '*':
+      print(firstVal * lastVal);
+elif operator == '/':
+      print(firstVal / lastVal);
+else:
+      print(f"Provided operator {operator} is not valid please try (+, -, * or /) ");
+
+
