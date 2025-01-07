1e32f6da2a4258d5b8d8482eefc3aee104b383c8
Muhammad Suleman <iam.suleman.m@gmail.com>
2025-01-07 23:56:24 +0500

    modify main branch code

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-#Simple Calculator
+# -- Simple Calculator -- #
+# firstVal = float(input("Add your first value: "));
+# lastVal = float(input("Add your last value: "));
+# operator = input("Add your operator(+ - / *): ");
+#
+# if operator == '+':
+#       print(firstVal + lastVal);
+# elif operator == '-':
+#       print(firstVal - lastVal);
+# elif operator == '*':
+#       print(firstVal * lastVal);
+# elif operator == '/':
+#       print(firstVal / lastVal);
+# else:
+#       print(f"Provided operator {operator} is not valid please try (+, -, * or /) ");
 
-firstVal = float(input("Add your first value: "));
-lastVal = float(input("Add your last value: "));
-operator = input("Add your operator(+ - / *): ");
-
-if operator == '+':
-      print(firstVal + lastVal);
-elif operator == '-':
-      print(firstVal - lastVal);
-elif operator == '*':
-      print(firstVal * lastVal);
-elif operator == '/':
-      print(firstVal / lastVal);
-else:
-      print(f"Provided operator {operator} is not valid please try (+, -, * or /) ");
 
 
+# -- Weight Converter -- #
+# weight = float(input("Add your weight: "));
+# weightType = input("Select your weight type(Kilogram -> K or Pound -> L): ");
+#
+# if weightType == "k" or weightType == "K":
+#     weight *=  2.205;
+#     unit = "LBs"
+#     print(f"Your weight is {round(weight, 1)} {unit}")
+# elif weightType == "L" or weightType == "l":
+#     weight /=  2.205;
+#     unit = "KGs"
+#     print(f"Your weight is {round(weight, 1)} {unit}")
+# else:
+#     print(f"Weight type {weightType} is invalid")
